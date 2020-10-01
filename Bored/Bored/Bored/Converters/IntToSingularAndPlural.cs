007ed6915c425c9f17aafd5c67cf4c98a0ce4fb1
Pär Jorwén <tjorw@hotmail.com>
2020-10-01 21:41:21 +0200

    value converter

---
+++ Bored/Bored/Bored/Converters/IntToSingularAndPlural.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bored.Converters
+{
+
+    public class IntToSingularAndPlural : IValueConverter, IMarkupExtension
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var intValue = (int)value;
+
+            if (intValue < 0) return "NEGATIVE";
+            if (intValue == 0) return "ZERO";
+            if (intValue == 1) return "ONE";
+
+            return "MANY";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 0;
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+
+
+}
--- Bored/Bored/Bored/MainPage.xaml.cs
+++ Bored/Bored/Bored/MainPage.xaml.cs
@@ -50,5 +50,6 @@ namespace Bored
         {
             await load();
         }
+
     }
 }
